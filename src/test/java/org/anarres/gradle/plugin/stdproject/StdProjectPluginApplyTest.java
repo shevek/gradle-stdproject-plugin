98d5b1f836b5f708db7f2682ca006afe507be5b9
Shevek <shevek@anarres.org>
2016-12-29 15:57:17 -0800

    Really clean up and test against multiple gradle versions.

--- src/test/java/org/anarres/gradle/plugin/stdproject/StdProjectPluginApplyTest.java
+++ src/test/java/org/anarres/gradle/plugin/stdproject/StdProjectPluginApplyTest.java
@@ -1,29 +1,76 @@
 package org.anarres.gradle.plugin.stdproject;
 
-import java.util.Collections;
-import org.gradle.api.Project;
-import org.gradle.testfixtures.ProjectBuilder;
+import com.google.common.io.Files;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author shevek
  */
+@RunWith(Parameterized.class)
 public class StdProjectPluginApplyTest {
 
-    Project project;
+    private static final Logger LOG = LoggerFactory.getLogger(StdProjectPluginApplyTest.class);
+
+    @Nonnull
+    private static Object[] A(Object... in) {
+        return in;
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> parameters() throws Exception {
+        return Arrays.asList(
+                A("2.12"),
+                A("2.14"),
+                A("3.0"),
+                A("3.2.1")
+        );
+    }
+
+    private final String gradleVersion;
+    @Rule
+    public final TemporaryFolder testProjectDir = new TemporaryFolder();
+    public File testProjectBuildFile;
 
     @Before
-    public void setUp() {
-        project = ProjectBuilder.builder().build();
+    public void setUp() throws Exception {
+        testProjectBuildFile = testProjectDir.newFile("build.gradle");
+    }
+
+    public StdProjectPluginApplyTest(String gradleVersion) {
+        this.gradleVersion = gradleVersion;
     }
 
     @Test
-    public void testApply() {
-        // project.apply(Collections.singletonMap("plugin", "java"));
-        // project.apply(Collections.singletonMap("plugin", "stdproject"));
-        // assertTrue("Project is missing plugin", project.getPlugins().hasPlugin(StdProjectPlugin.class));
+    public void testApply() throws Exception {
+        String text = "plugins { id 'org.anarres.stdproject' }\n"
+                + "stdproject {}\n";
+        Files.write(text, testProjectBuildFile, StandardCharsets.UTF_8);
+
+        GradleRunner runner = GradleRunner.create()
+                .withGradleVersion(gradleVersion)
+                .withPluginClasspath()
+                .withDebug(true)
+                .withProjectDir(testProjectDir.getRoot())
+                .withArguments("tasks");
+        LOG.info("Building...\n\n");
+        // System.out.println("ClassPath is " + runner.getPluginClasspath());
+        BuildResult result = runner.build();
+        LOG.info("Output:\n\n" + result.getOutput() + "\n\n");
     }
+
 }
